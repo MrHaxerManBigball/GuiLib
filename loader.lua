ebbd98df7e7dd1e55fb4cedef14d10308baa4780
MrHaxerManBigball <96355277+MrHaxerManBigball@users.noreply.github.com>
2023-05-13 13:46:37 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,8 @@
 repeat task.wait() until game:IsLoaded() 
 if isfolder("testlib") and isfile("testlib/games/"..game.PlaceId..".lua") then 
 	loadstring(readfile("testlib/games/"..game.PlaceId..".lua"))()
-	elseif not game:HttpGet("https://raw.githubusercontent.com/MrHaxerManBigball/GuiLib/main/games/"..game.PlaceId) == "404: Not Found" then 
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/MrHaxerManBigball/GuiLib/main/games/"..game.PlaceId, true))()
+	elseif not game:HttpGet("https://raw.githubusercontent.com/MrHaxerManBigball/GuiLib/main/games/"..game.PlaceId..".lua") == "404: Not Found" then 
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/MrHaxerManBigball/GuiLib/main/games/"..game.PlaceId..".lua", true))()
 	else 
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/MrHaxerManBigball/GuiLib/main/anyGame", true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/MrHaxerManBigball/GuiLib/main/anyGame.lua", true))()
 end 
